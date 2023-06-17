1dfd9e7963ef3bd100e43958d30794000d41b8b4
thisIsBun <bun.coding@gmail.com>
2023-06-17 14:07:35 +0800

    initialize dist folder, cause deploy face issue

--- vite.config.js
+++ vite.config.js
@@ -18,21 +18,4 @@ export default defineConfig({
             },
         },
     },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: (id) => {
-                    if (id.includes('node_modules/react') || id.includes('node_modules/react-dom') || id.includes('node_modules/react-router-dom')) {
-                        return 'react';
-                    } else if (id.includes('node_modules/@fortawesome/free-solid-svg-icons')) {
-                        return 'fortawesome';
-                    } else if (id.includes('node_modules/@reach/combobox') || id.includes('node_modules/@react-google-maps/api') || id.includes('node_modules/use-places-autocomplete') || id.includes('node_modules/firebase')) {
-                        return 'google';
-                    } else if (id.includes('node_modules/@fortawesome/fontawesome-svg-core') || id.includes('node_modules/react-table') || id.includes('node_modules/styled-components' || id.includes('node_modules/sweetalert2')) || id.includes('node_modules/@fortawesome/react-fontawesome') || id.includes('node_modules/@fortawesome/free-regular-svg-icons')) {
-                        return 'utilities';
-                    }
-                }
-            }
-        }
-    }
 })
