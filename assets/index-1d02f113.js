1de51db5d85a75181a40b2560e50d47e56b8dac5
thisIsBun <bun.coding@gmail.com>
2023-06-19 17:46:44 +0800

    Updates

--- assets/index-1d02f113.js
+++
@@ -1,6 +0,0 @@
-import{r as t,h as l,j as n}from"./react-b7acef7a.js";import{F as f}from"./Form-d225da22.js";import{s as x,c as S}from"./vendor-9071635d.js";import{A,M as E,g as w,b as s,d as U,s as j}from"./index-81d00f9c.js";import"./google-6ac6e1d4.js";import"./fortawesome-4f418b12.js";const v=x.div`
-  ${E} {
-    width: 478px;
-    margin: 0 auto;
-  }
-`;function L(){const[e,i]=t.useState(""),[a,u]=t.useState(""),[c,r]=t.useState(""),[g,o]=t.useState(!1),{setUser:m}=t.useContext(A),d=l(),p=async M=>{o(!0),w("event","sign_up",{method:"Google"});try{await S(s,e,a),U.fire({title:"註冊成功"}),j(s.currentUser.uid),m(s.currentUser.uid),d("/")}catch(h){r(h.message),o(!1)}};return n.jsx(v,{children:n.jsx(f,{title:"Sign up",btnName:"註冊",account:e,password:a,setAccount:i,setPassword:u,handleSignup:p,errorMessage:c,setErrorMessage:r,isLoading:g})})}export{L as default};
--- assets/index-980037fb.js
+++
@@ -1,6 +0,0 @@
-import{r as t,h,j as n}from"./react-b7acef7a.js";import{A as f,M as x,g as A,b as s,d as E,s as w}from"./index-81d00f9c.js";import{F as L}from"./Form-d225da22.js";import{s as j,b as v}from"./vendor-9071635d.js";import"./google-6ac6e1d4.js";import"./fortawesome-4f418b12.js";const M=j.div`
-  ${x} {
-    width: 478px;
-    margin: 0 auto;
-  }
-`;function P(){const[e,i]=t.useState(""),[a,u]=t.useState(""),[c,o]=t.useState(""),[g,r]=t.useState(!1),{setUser:m}=t.useContext(f),d=h(),l=async()=>{r(!0),A("event","login_email",{method:"Google"});try{await v(s,e,a),E.fire({title:"登入成功"}),w(s.currentUser.uid),m(s.currentUser.uid),d("/")}catch(p){o(p.message),r(!1)}};return n.jsx(M,{children:n.jsx(L,{title:"Log in",btnName:"登入",account:e,password:a,setAccount:i,setPassword:u,handleLogin:l,errorMessage:c,setErrorMessage:o,isLoading:g})})}export{P as default};
