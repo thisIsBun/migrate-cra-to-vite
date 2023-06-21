e34200fe0cddd65ed252ec07bdd6c7fe9546a21c
thisIsBun <bun.coding@gmail.com>
2023-06-21 15:05:03 +0800

    debug: fix %PUBLIC_URL% by @rollup/plugin-replace

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
+import replace from '@rollup/plugin-replace';
 
 export default defineConfig({
     server: {
         open: '/index.html'
     },
-    plugins: [react()],
+    plugins: [replace({
+        __PUBLIC_URL__: '/migrate-cra-to-vite',
+        preventAssignment: true,
+    }), react()],
     esbuild: {
         loader: "jsx",
         include: /src\/.*\.jsx?$/,
